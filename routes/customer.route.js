2c869c0c015a853927ffe18a6415f4b43030389d
t0dida00 <t0dida00@students.oamk.fi>
2021-04-06 09:55:23 +0300

    Edit customer service

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const productModel = require('../models/product.model');
+const customerModel = require('../models/customer.model');
 
 const router = express.Router();
 
-// router.get('/', async function (req, res) {
-//   const list = await productModel.all();
-//   res.render('vwProducts/list', {
-//     products: list,
-//     empty: list.length === 0
-//   });
-// })
-
-router.get('/',function(req,res){
-    res.render('vwCustomers/list');
+router.get('/', async function (req, res) {
+  const list = await customerModel.all();
+  res.render('vwCustomers/list', {
+    customers: list,
+    empty: list.length === 0,
+    err:req.session.err,
+    success: req.session.success
+    
+  });
+ 
+  delete req.session.err
+  delete req.session.success
 })
+router.get('/edit/:id', async function (req, res) {
+    // const id = +req.query.id || -1;
+    const id = +req.params.id || -1;
+  
+    const rows = await customerModel.single(id);
+    if (rows.length === 0)
+      return res.send('Invalid parameter.');
+  
+    const customer = rows[0];
+    res.render('vwCustomers/edit', { customer });
+  })
+
+  router.post('/update', async function (req, res) {
+    
+    await customerModel.patch(req.body);
+    req.session.success="Update customer information successfully !"
+    res.redirect('/admin/customers');
+  })
+  
 module.exports = router;
