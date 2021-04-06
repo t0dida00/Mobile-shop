f1388a96acef4075bea3a596d08cb60f43d16dcf
t0dida00 <t0dida00@students.oamk.fi>
2021-04-06 09:35:52 +0300

    j
    j

---
+++ models/customer.model.js
@@ -0,0 +1,36 @@
+const db = require('../utils/db');
+
+const TBL_CATEGORIES = 'customers';
+
+module.exports = {
+  all: function () {
+    return db.load(`select * from ${TBL_CATEGORIES}`);
+  },
+  single: function (id) {
+    return db.load(`select * from ${TBL_CATEGORIES} where customer_id = ${id}`);
+  },
+  add: function (entity) {
+    return db.add(TBL_CATEGORIES, entity);
+  },
+  patch: function (entity) {
+    const condition = {
+      customer_id: entity.customer_id
+    }
+
+    delete entity.customer_id;
+    return db.patch(TBL_CATEGORIES, entity, condition);
+  },
+  del: function (id) {
+    
+    const condition = {
+      customer_id: id
+    }  
+    return db.del(TBL_CATEGORIES, condition);
+  },
+  available: function(name)
+  {
+    
+    return db.load(`select * from ${TBL_CATEGORIES} where customer_id_name = "${name}"`);
+  }
+  
+};
