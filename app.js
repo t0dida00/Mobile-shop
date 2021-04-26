967e5dc9dfe9c63955c5389aa3209ba99b60b9b0
t0dida00 <t0dida00@students.oamk.fi>
2021-04-26 16:25:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(function(req,res,next){
   if(!req.session.isAuthenticated)
   {
 
-    return res.render("vwAccount/login")
+    return res.render("vwAccount/login",  {layout: false})
   }
   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   next();
